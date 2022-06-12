076a4bb38ca53959a25b456a6975554db3c75ce5
asepjayamutakin12@gmail.com <rivaldwirexy@gmail.com>
2022-06-12 21:51:37 +0700

    firstCommitTugas

--- promise.js
+++ promise.js
@@ -1,7 +1,35 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    let promiseTheater1 = await promiseTheaterIXX();
+    let promiseTheater2 = await promiseTheaterVGC();
+    let promiseTheaterArr = [];
+    let marahTotal = 0;
+    let tidakMarahTotal = 0;
+    for (let counter = 0; counter < promiseTheater1.length; counter++) {
+      promiseTheaterArr.push(promiseTheater1[counter].hasil);
+    }
+    for (let counter = 0; counter < promiseTheater2.length; counter++) {
+      promiseTheaterArr.push(promiseTheater2[counter].hasil);
+    }
+    for (let counter = 0; counter < promiseTheaterArr.length; counter++) {
+      if (promiseTheaterArr[counter] === "marah") {
+        marahTotal++;
+      } else {
+        tidakMarahTotal++;
+      }
+    }
+    if (emosi === "marah") {
+      return marahTotal;
+    } else {
+      return tidakMarahTotal;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
